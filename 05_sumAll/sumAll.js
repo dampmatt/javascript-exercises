ca7404530442130fece93abc77d5238b2171ada2
Utkarsh <utkarshchoudhary405@gmail.com>
2024-06-16 05:13:59 +0530

    completed 5th exercise

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,14 @@
-const sumAll = function() {
-
+const sumAll = function(a,b) {
+    if(a<0 || b<0 || !Number.isInteger(a) || !Number.isInteger(b))
+        return "ERROR";
+    start=Math.min(a,b);
+    end=Math.max(a,b);
+    let sum=0;
+    while(start<=end){
+        sum+=start;
+        start++;
+    }
+    return sum;
 };
 
 // Do not edit below this line
