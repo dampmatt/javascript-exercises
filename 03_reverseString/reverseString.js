d57fbf20b5d9d67100868a37e5d206c46917fdbb
Utkarsh <utkarshchoudhary405@gmail.com>
2024-06-16 04:26:54 +0530

    completed 3rd exercise

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    let temp="";
+    i=str.length
+    while(i--){
+        temp+=str[i];
+    }
+    return temp;
 };
 
 // Do not edit below this line
