2bb67e0fd8e03ed49e878e6b3b66b54b51292720
Utkarsh <utkarshchoudhary405@gmail.com>
2024-06-16 04:50:20 +0530

    completed 6th exercise

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,14 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr,ele) {
+    let temp= [1,2,3,4];
+    for(const i of arr){
+        if(i!=ele)
+            temp+=i;
+        // console.log(i+" ");
+    }
+    console.log(typeof(temp));
+    return arr;
 };
 
+removeFromArray([1,2,4,3],3);
 // Do not edit below this line
 module.exports = removeFromArray;
