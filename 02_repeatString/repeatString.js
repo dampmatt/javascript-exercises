6fd8f554b450ab453928e9a99e17c1d49eb4d0b3
Utkarsh <utkarshchoudhary405@gmail.com>
2024-06-16 04:17:19 +0530

    completed 2nd exercise

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
-
+const repeatString = function(word,num) {
+    if(num<0)
+        return "ERROR";
+    let str="";
+    let i=0;
+    while(i<num){
+        str+=word;  
+        i++;  
+    }
+    // console.log(num);
+    return str;
 };
 
 // Do not edit below this line
